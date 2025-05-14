74d5f20aa3cedd84332fbfa2ade5832e0a30ad67
AbdulazizNosirjonov <89240483+AbdulazizNosirjonov@users.noreply.github.com>
2025-05-14 19:07:04 +0500

    MINOR: error fixed.

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,4 +1,3 @@
-
 @props(['messages'])
 
 @if ($messages)
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,9 +15,6 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('authors')" :active="request()->routeIs('authors')">
-                         Authors
-                    </x-nav-link>
                 </div>
             </div>
 
